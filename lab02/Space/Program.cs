21085bcb0ca14c7ef2d316dfa33e22c36fc21e21
1nevi <ziniewicz.p@gmail.com>
2016-05-08 01:06:01 +0200

    Introduction to WCF services

---
+++ lab02/Space/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri address = new Uri("http://localhost:9009/Space");
+
+            ServiceHost selfHost = new ServiceHost(typeof(BlackHole), address);
+
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(IBlackHole), new WSHttpBinding(), "SpaceServiceEndpoint");
+
+                ServiceMetadataBehavior smd = new ServiceMetadataBehavior();
+                smd.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smd);
+
+                selfHost.Open();
+                Console.WriteLine("Service is running!");
+                Console.ReadLine();
+                selfHost.Close();
+            }
+
+            catch (CommunicationException ex)
+            {
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+
+    public class Person
+    {
+        public string Name;
+        public int Age;
+    }
+
+    public class Starship
+    {
+        public string Name;
+        public Person Captain;
+        public List<Person> Crew;
+    }
+
+    public class Planet
+    {
+        public string Name;
+        public int Mass;
+    }
+
+    [ServiceContract]
+    public interface IBlackHole
+    {
+        [OperationContract]
+        Starship PullStarship(Starship ship);
+
+        [OperationContract]
+        string UltimateAnswer();
+    }
+
+    public class BlackHole : IBlackHole
+    {
+        public Starship PullStarship(Starship ship)
+        {
+            if( ship.Captain.Age < 40 )
+            {
+                foreach(Person pers in ship.Crew)
+                {
+                    pers.Age = pers.Age + 20;
+                }
+            }
+
+            return ship;
+        }
+
+        public string UltimateAnswer()
+        {
+            return 42.ToString();
+        }
+    }
+}
---
+++ lab02/SpaceClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using SpaceClient.ServiceReference1;
+using System;
+using System.Linq;
+
+
+namespace SpaceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlackHoleClient bhc = new BlackHoleClient();
+
+            Starship ship1 = new Starship() { Name = "Statek mocy", Captain = new Person() { Name = "Piotr", Age = 35 } };
+            ship1.Crew = new Person[] { new Person() { Name = "Maciej", Age = 25 }, new Person() { Name = "Diana", Age = 26 }, new Person() { Name = "Łukasz", Age = 20 }, new Person() { Name = "Stanisław", Age = 50 }, new Person() { Name = "Aleksandra", Age = 33 } };
+
+            Console.WriteLine("Przed PullStarship:");
+            PresentCrew(ship1);
+
+            Starship ship2 = bhc.PullStarship(ship1);
+
+            Console.WriteLine("Po PullStarship:");
+            PresentCrew(ship2);
+
+            Console.WriteLine("Ultimate answer: {0}", bhc.UltimateAnswer());
+
+            Console.ReadLine();
+        }
+
+        public static void PresentCrew(Starship starship)
+        {
+            Console.WriteLine("Kapitan:");
+            Console.WriteLine("Imię: {0}, Wiek: {1}", starship.Captain.Name, starship.Captain.Age);
+            Console.WriteLine();
+            Console.WriteLine("Załoga:");
+            
+            foreach (Person pers in starship.Crew)
+            {
+                Console.WriteLine("Imię: {0}, Wiek: {1}", pers.Name, pers.Age);
+            }
+            Console.WriteLine();
+        }
+    }
+}
